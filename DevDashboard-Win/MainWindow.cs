8b5679685ceaaa8209c8247dc02af96973c09b73
Eric Hooks <ebhooks21@gmail.com>
2025-01-26 23:22:52 -0600

    Setup code to display the welcome message.

--- DevDashboard-Win/MainWindow.cs
+++ DevDashboard-Win/MainWindow.cs
@@ -1,10 +1,41 @@
+/**
+ * MainWindow.cs -- File to contain the controls for the main WinForms menu.
+ * @author Eric Hooks, 2025
+ */
 namespace DevDashboard_Win; 
+
+/**
+ * MainWindow class.
+ */
 public partial class MainWindow : Form {
+
+	/**
+	 * Main constructor for the MainWindow.
+	 */
 	public MainWindow() {
 		InitializeComponent();
 	}
 
+	/**
+	 * Method that is called when the MainWindow is loaded.
+	 */
 	private void MainWindow_Load(object sender, EventArgs e) {
-		WelcomeMsgArea.Text = "Good Morning, Eric Hooks.";
+		//Get the current time
+		DateTime dateTime = DateTime.Now;
+		string welcomeMsg = string.Empty;
+
+		if (dateTime.Hour < 12) {
+			welcomeMsg = "Good Morning, ";
+		}
+
+		else if ((dateTime.Hour > 12) && (dateTime.Hour <= 17)) {
+			welcomeMsg = "Good Afternoon, ";
+		}
+
+		else {
+			welcomeMsg = "Good Evening, ";
+		}
+
+		WelcomeMsgArea.Text = welcomeMsg + "Eric Hooks.";
 	}
 }
