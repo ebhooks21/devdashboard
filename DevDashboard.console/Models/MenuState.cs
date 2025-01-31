5f33211ca1c7d0d4f311d5f4c028deb37b0ab74b
Eric Hooks <ebhooks21@gmail.com>
2025-01-30 20:41:15 -0600

    Setup an enum to track the menu state in the console.

---
+++ DevDashboard.console/Models/MenuState.cs
@@ -0,0 +1,12 @@
+/**
+* MenuState.cs -- File to hold the enum for the menu state.
+* @author Eric Hooks, 2025
+*/
+namespace DevDashboard.console.Models;
+
+/**
+* Enum for the menu state.
+*/
+public enum MenuState {
+	MainMenu
+}
